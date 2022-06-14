22dafbf51b8c63bbaa10265b1325fde449c4282a
jaybitdesign <jaybitdesign@gmail.com>
2022-06-14 13:35:06 -0600

    Manually replaced files (version control)

---
+++ CPNT-262 Node.js/Node.js Final Project 2.0/server.js
@@ -0,0 +1,523 @@
+// Import required libraries
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser")
+const mysql = require("mysql")
+
+
+// Set up Express.js
+const app = express();
+
+// Express middleware for parsing bodies from URL
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Use the files in the "public" folder
+app.use(express.static("public"));
+
+// Set the view engine to use .pug templates
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// Misc. site configuration
+const brand = "Travel Experts";
+
+// Initialize server variables
+const port = 8000;
+var server = null;
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "Admin",
+    password: "Password123",
+    database: "travelexperts"
+});
+
+// Start the server and listen on the chosen {_port}
+function startServer(_port) {
+    server = app.listen(_port, () => { console.log("Server started on port: " + _port); })
+}
+
+// Render the main index view template
+app.get(["/", "/index"], (req, res) => {
+
+
+    // Define SQL statement(s)
+    var packageSQL =
+        "SELECT * " +
+        "FROM packages";
+
+    // Open a new MYSQL query with our SQL statement
+    con.query(packageSQL, (err, result) => {
+
+        // Catch any errors from the query callback
+        if (err) throw err;
+
+        // Log the query result to console
+        console.log(result);
+
+        // Render the bookings view template with the {result} passed in
+        res.render("index", {
+            _title: "Travel Experts",
+            _brand: brand,
+            result: result
+        });
+    });
+})
+// render agencies view template from db    
+app.get("/agencies", (req, res) => {
+    var agenciesSQL =
+    "SELECT * " + 
+    "FROM agencies";
+    con.query(agenciesSQL, (err, result) => {
+        if (err) throw err;
+        console.log(result)
+        res.render("agencies", {
+            _title: "Agencies",
+            _brand: brand,
+            result: result
+        })
+    })
+
+})
+// Render contact view template
+app.get("/contact", (req, res) => {
+
+    // Define SQL statement(s)
+    var contactsSQL =
+        "SELECT * " +
+        "FROM agents";
+
+    // Open a new MYSQL query with our SQL statement
+    con.query(contactsSQL, (err, result) => {
+
+        // Catch any errors from the query callback
+        if (err) throw err;
+
+        // Log the query result to console
+        console.log(result);
+
+        // Render the contact view template with the {result} passed in
+        res.render("contact", {
+            _title: "Contact Us",
+            _brand: brand,
+            result: result
+        });
+    });
+})
+
+// Render the thanks view template with result from the register query
+app.get("/thanks", (req, res) => {
+    res.render("thanks", {
+        _title: "Thank You!",
+        _brand: brand,
+        fullname: req.query.fullname,
+        email: req.query.email,
+        postal: req.query.postal
+    });
+});
+
+// Render the bookings view template
+app.get("/bookings", (req, res) => {
+
+    // Define SQL statement(s)
+    var bookingsSQL = "SELECT * FROM `bookings`";
+
+    // Open a new MYSQL query with our SQL statement
+    con.query(bookingsSQL, (err, result) => {
+
+        // Catch any errors from the query callback
+        if (err) throw err;
+
+        // Log the query result to console
+        console.log(result);
+
+        // Render the bookings view template with the {result}
+        res.render("bookings", {
+            _title: "Bookings",
+            _brand: brand,
+            result: result
+        })
+    });
+})
+
+// Render the booking view template
+app.get("/booking", (req, res) => {
+
+    // Define SQL statement(s)
+    var customerSQL = "SELECT CustomerId, CustomerName from customers";
+    var packageSQL = "SELECT PackageId, PackageName, PackageDesc FROM packages WHERE PackageId=?";
+
+    // Open a new MYSQL query with our SQL statement
+    con.query(customerSQL, (err, customers) => {
+
+        // Catch any errors from the query callback
+        if (err) throw err;
+
+        // Log the query result to console
+        console.log(customers);
+
+        // Store the CustomerId from the query in a variable
+        var packageId = req.query.packageId;
+
+        con.query({ sql: packageSQL, values: [packageId] }, (err, packages) => {
+
+            // Catch any errors from the query callback
+            if (err) throw err;
+
+            // Log the query result to console
+            console.log(packages);
+
+            // Render the update-customer view template with {customersResult} and {agentResult}
+            res.render("booking", {
+                _title: "New Booking",
+                _brand: brand,
+                customers: customers,
+                packages: packages
+            })
+        });
+    });
+});
+
+// Render the insert-customer-post view template
+app.post("/booking-post", (req, res) => {
+
+    // Define SQL statement(s)
+    var insertSQL =
+        "INSERT INTO bookings" +
+        "(BookingDate, CustomerId, PackageId) " +
+        "VALUES (?,?,?)";
+
+    // Store values from the URL body in an array
+    var values = [
+        new Date(),
+        req.body.CustomerId,
+        req.body.PackageId
+    ];
+
+    // Open a new MYSQL query with our SQL statement
+    con.query(insertSQL, values, (err, result) => {
+
+        // Catch any errors from the query callback
+        if (err) throw err;
+
+        // Log the query result to console
+        console.log(result);
+
+        // Display the amount of rows inserted
+        if (result.affectedRows) { res.status(200).redirect("bookings") }
+
+        else { res.status(200).send("Insert failed!"); }
+
+    });
+});
+
+// Render the display-customer view template
+app.get("/display-customer", (req, res) => {
+
+    // Open a new MYSQL query with our SQL statement
+    con.query("SELECT CustomerId, CustomerName from Customers", (err, result, fields) => {
+
+        // Catch any errors from the query callback
+        if (err) throw err;
+
+        // Log the query result to console
+        console.log(result);
+
+        // Render the display-customer view template with the {result}
+        res.render("display-customer", {
+            _title: "Display Customer",
+            _brand: brand,
+            result: result,
+            fields: fields
+        });
+    });
+});
+
+// Render the display-customer-get view template
+app.get("/display-customer-get", (req, res) => {
+
+    // Store CustomerId from the query to a variable
+    var customerId = req.query.CustomerId;
+
+    // Open a new MYSQL query with our SQL statement, and pass in CustomerId from query
+    con.query({
+        sql: "SELECT * FROM customers WHERE CustomerId=?",
+        values: [customerId]
+    }, (err, result, fields) => {
+
+        // Catch any errors from the query callback
+        if (err) throw err;
+
+        // Log the query result to console
+        console.log(result);
+
+        // Log the query result to console
+        console.log(result, fields);
+
+        // Render the customer view template with the {result}
+        res.render("customer", {
+            _title: "Display Customer",
+            _brand: brand,
+            result: result,
+            fields: fields
+        });
+    });
+});
+
+// Render the display-customer-get-all view template
+app.get("/display-customer-get-all", (req, res) => {
+
+    // Open a new MYSQL query with our SQL statement
+    con.query("SELECT * FROM customers", (err, result, fields) => {
+
+        // Catch any errors from the query callback
+        if (err) throw err;
+
+        // Log the query result to console
+        console.log(result);
+
+        // Render the customer view template with the {result}
+        res.render("customer", {
+            _title: "Display Customer",
+            _brand: brand,
+            result: result,
+            fields: fields
+        });
+    });
+});
+
+// Render the update-customer view template
+app.get("/update-customer", (req, res) => {
+
+    // Open a new MYSQL query with our SQL statement
+    con.query("SELECT CustomerId, CustomerName from Customers", (err, result, fields) => {
+
+        // Catch any errors from the query callback
+        if (err) throw err;
+
+        // Log the query result to console
+        console.log(result);
+
+        // Render the update-customer-select view template with the {result}
+        res.render("update-customer-select", {
+            _title: "Update Customer",
+            _brand: brand,
+            result: result,
+            fields: fields
+        });
+    });
+})
+
+// Render the update-customer-get view template
+app.get("/update-customer-get", (req, res) => {
+
+    // Define SQL statement(s)
+    var customerSQL =
+        "SELECT * " +
+        "FROM customers " +
+        "WHERE CustomerID=?";
+
+    // Store the CustomerId from the query in a variable
+    var customerId = req.query.CustomerId;
+
+    con.query({ sql: customerSQL, values: [customerId] }, (err, result) => {
+
+        // Catch any errors from the query callback
+        if (err) throw err;
+
+        // Log the query result to console
+        console.log(result);
+
+        // Render the update-customer view template with {customersResult} and {agentResult}
+        res.render("update-customer", {
+            _title: "Update Customer",
+            _brand: brand,
+            customers: result,
+        })
+    });
+});
+
+// Render the update-customer-post view template
+app.post("/update-customer-post", (req, res) => {
+
+    // Define SQL statement(s)
+    var sql =
+        "UPDATE `customers` " +
+        "SET `CustomerName`=?,`CustomerPostal`=?,`CustomerPassword`=?,`CustomerEmail`=? " +
+        "WHERE `CustomerId`=?";
+
+    // Store values from the URL body in an array
+    var values = [
+        req.body.CustomerName,
+        req.body.CustomerPostal,
+        req.body.CustomerPassword,
+        req.body.CustomerEmail,
+        req.body.CustomerId
+    ];
+
+    // Log the query result to console
+    console.log(values);
+
+    // Open a new MYSQL query with our SQL statement
+    con.query(sql, values, (err, result) => {
+
+        // Catch any errors from the query callback
+        if (err) throw err;
+
+        // Log the query result to console
+        console.log("Updated " + result.affectedRows + " row(s).");
+
+        // Display the amount of rows updated
+        if (result.affectedRows) { res.status(200).redirect("display-customer-get-all"); }
+
+        // Display an error
+        else { res.status(200).send("Update failed!"); }
+
+    });
+});
+
+// custdelete
+app.get("/delete-customer", (req, res) => {
+
+    // Define SQL statement(s)
+    var deleteSQL =
+        "SELECT CustomerId, CustomerName, CustomerPostal, CustomerEmail, CustomerPassword " +
+        "FROM customers";
+
+    // Open a new MYSQL query with our SQL statement
+    con.query(deleteSQL, (err, result, fields) => {
+
+        // Catch any errors from the query callback
+        if (err) throw err;
+
+        // Render the delete-customer-select view template with {result}
+        res.render("delete-customer-select", {
+            _title: "Delete Customer",
+            _brand: brand,
+            result: result,
+            fields: fields
+        });
+    });
+});
+
+// Render the delete-customer-get view template
+app.get("/delete-customer-get", (req, res) => {
+
+    // Define SQL statement(s)
+    var agentSQL =
+        "SELECT AgentID, AgentFirstName, AgentLastName " +
+        "FROM AGENTS";
+    var customerSQL =
+        "SELECT * from customers " +
+        "WHERE CustomerId=?";
+
+    // Open a new MYSQL query with our SQL statement
+    con.query(agentSQL, (err, _agentResult, fields) => {
+
+        // Catch any errors from the query callback
+        if (err) throw err;
+
+        // Store the CustomerId from the query in a variable
+        var customerID = req.query.CustomerID;
+
+        con.query({ sql: customerSQL, values: [customerID] }, (err, _result) => {
+
+            // Catch any errors from the query callback
+            if (err) throw err;
+
+            // Log the query result to console
+            console.log(_result);
+
+            // Render the delete-customer view template with {agentResult} and {result}
+            res.render("delete-customer", {
+                _title: "Delete Customer",
+                _brand: brand,
+                agentResult: _agentResult,
+                result: _result
+            });
+        });
+    });
+});
+
+// Render the delete-customer-post view template
+app.post("/delete-customer-post", (req, res) => {
+
+    // Define SQL statement(s)
+    var deleteSQL = "DELETE FROM `customers` WHERE CustomerId=?";
+
+    // Store the CustomerId from the URL body in a variable
+    var values = [req.body.CustomerId];
+
+    // Open a new MYSQL query with our SQL statement
+    con.query(deleteSQL, values, (err, result) => {
+
+        // Catch any errors from the query callback
+        if (err) throw err;
+
+        // Log the query result to console
+        console.log(result);
+
+        // Display the amount of rows deleted
+        if (result.affectedRows) { res.status(200).redirect("display-customer-get-all"); }
+
+        // Display an error
+        else { res.status(200).send("Delete failed!"); }
+
+    });
+});
+
+// Render the insert-customer view template
+app.get("/insert-customer", (req, res) => {
+
+    // Render the insert-customer view template with {result}
+    res.render("insert-customer", {
+        _title: "Insert Customer",
+        _brand: brand
+    });
+});
+
+// Render the insert-customer-post view template
+app.post("/insert-customer-post", (req, res) => {
+
+    // Define SQL statement(s)
+    var insertSQL =
+        "INSERT INTO customers" +
+        "(CustomerName, CustomerPostal, CustomerEmail, CustomerPassword) " +
+        "VALUES (?,?,?,?)";
+
+    // Store values from the URL body in an array
+    var values = [
+        req.body.CustomerName,
+        req.body.CustomerPostal,
+        req.body.CustomerEmail,
+        req.body.CustomerPassword
+    ];
+
+    // Open a new MYSQL query with our SQL statement
+    con.query(insertSQL, values, (err, result) => {
+
+        // Catch any errors from the query callback
+        if (err) throw err;
+
+        // Log the query result to console
+        console.log(result);
+
+        // Display the amount of rows inserted
+        if (result.affectedRows) { res.status(200).redirect("display-customer-get-all"); }
+        else { res.status(200).send("Insert failed!"); }
+
+    });
+});
+
+// Render error view template on 404 error
+app.use((req, res, next) => {
+    res.status(404).render("404", {
+        _title: "404 Page Not Found",
+        _brand: brand
+    })
+})
+
+// Start the server @ {port}
+startServer(port);
+
+
+
---
+++ CPNT-262 Node.js/Node.js Final Project 2.0/travelexperts.sql
@@ -0,0 +1,293 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 14, 2022 at 09:31 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `travelexperts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `agencies`
+--
+
+CREATE TABLE `agencies` (
+  `AgencyId` int(11) NOT NULL,
+  `AgncyPhoto` varchar(500) NOT NULL,
+  `AgncyAddress` varchar(50) DEFAULT NULL,
+  `AgncyCity` varchar(50) DEFAULT NULL,
+  `AgncyProv` varchar(50) DEFAULT NULL,
+  `AgncyPostal` varchar(50) DEFAULT NULL,
+  `AgncyCountry` varchar(50) DEFAULT NULL,
+  `AgncyPhone` varchar(50) DEFAULT NULL,
+  `AgncyFax` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `agencies`
+--
+
+INSERT INTO `agencies` (`AgencyId`, `AgncyPhoto`, `AgncyAddress`, `AgncyCity`, `AgncyProv`, `AgncyPostal`, `AgncyCountry`, `AgncyPhone`, `AgncyFax`) VALUES
+(1, 'https://loremflickr.com/800/500/travel', '1155 8th Ave SW', 'Calgary', 'AB', 'T2P1N3', 'Canada', '4032719873', '4032719872'),
+(2, 'https://loremflickr.com/800/500/plane', '110 Main Street', 'Okotoks', 'AB', 'T7R3J5', 'Canada', '4035632381', '4035632382');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `agents`
+--
+
+CREATE TABLE `agents` (
+  `AgentId` int(11) NOT NULL,
+  `AgentFirstName` varchar(20) DEFAULT NULL,
+  `AgentMiddleInitial` varchar(5) DEFAULT NULL,
+  `AgentLastName` varchar(20) DEFAULT NULL,
+  `AgentPhoto` varchar(256) DEFAULT NULL,
+  `AgentBusPhone` varchar(20) DEFAULT NULL,
+  `AgentEmail` varchar(50) DEFAULT NULL,
+  `AgentPosition` varchar(20) DEFAULT NULL,
+  `AgencyId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `agents`
+--
+
+INSERT INTO `agents` (`AgentId`, `AgentFirstName`, `AgentMiddleInitial`, `AgentLastName`, `AgentPhoto`, `AgentBusPhone`, `AgentEmail`, `AgentPosition`, `AgencyId`) VALUES
+(1, 'Janet', NULL, 'Delton', 'https://place-hold.it/320x320.png&text=Contact%20#1', '(403) 210-7801', 'janet.delton@travelexperts.com', 'Senior Agent', 1),
+(2, 'Judy', NULL, 'Lisle', 'https://place-hold.it/320x320.png&text=Contact%20#2', '(403) 210-7802', 'judy.lisle@travelexperts.com', 'Intermediate Agent', 1),
+(3, 'Dennis', 'C.', 'Reynolds', 'https://place-hold.it/320x320.png&text=Contact%20#3', '(403) 210-7843', 'dennis.reynolds@travelexperts.com', 'Junior Agent', 1),
+(4, 'John', NULL, 'Coville', 'https://place-hold.it/320x320.png&text=Contact%20#4', '(403) 210-7823', 'john.coville@travelexperts.com', 'Intermediate Agent', 1),
+(5, 'Fred', 'J', 'Smith', 'https://place-hold.it/320x320.png&text=Contact%20#5', '(403) 210-5555', 'fred@travelexperts.com', 'Junior Agent', 2),
+(6, 'Bruce', 'J.', 'Dixon', 'https://place-hold.it/320x320.png&text=Contact%20#6', '(403) 210-7867', 'bruce.dixon@travelexperts.com', 'Intermediate Agent', 2),
+(7, 'Beverly', 'S.', 'Jones', 'https://place-hold.it/320x320.png&text=Contact%20#7', '(403) 210-7812', 'beverly.jones@travelexperts.com', 'Intermediate Agent', 1),
+(8, 'Jane', NULL, 'Merrill', 'https://place-hold.it/320x320.png&text=Contact%20#8', '(403) 210-7868', 'jane.merrill@travelexperts.com', 'Senior Agent', 2),
+(9, 'Brian', 'S.', 'Peterson', 'https://place-hold.it/320x320.png&text=Contact%20#9', '(403) 210-7833', 'brian.peterson@travelexperts.com', 'Junior Agent', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `BookingId` int(11) NOT NULL,
+  `BookingDate` datetime DEFAULT NULL,
+  `CustomerId` int(11) DEFAULT NULL,
+  `TripTypeId` varchar(1) DEFAULT NULL,
+  `PackageId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`BookingId`, `BookingDate`, `CustomerId`, `TripTypeId`, `PackageId`) VALUES
+(1326, '2022-06-14 00:26:00', 130, NULL, 1),
+(1327, '2022-06-14 00:32:59', 130, NULL, 5),
+(1328, '2022-06-14 10:39:12', 109, NULL, 1),
+(1329, '2022-06-14 12:04:22', 130, NULL, 2),
+(1330, '2022-06-14 12:05:09', 152, NULL, 2),
+(1331, '2022-06-14 12:34:58', 107, NULL, 2),
+(1332, '2022-06-14 12:54:59', 152, NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `CustomerId` int(11) NOT NULL,
+  `CustomerName` varchar(50) NOT NULL,
+  `CustomerPostal` varchar(7) NOT NULL,
+  `CustomerEmail` varchar(50) NOT NULL,
+  `CustomerPassword` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`CustomerId`, `CustomerName`, `CustomerPostal`, `CustomerEmail`, `CustomerPassword`) VALUES
+(107, 'Catherine', 'T2Z 2Z9', 'cmierzwa@msn.com                                  ', '12345'),
+(108, 'Judy', 'T1Y 6N4', 'judysehti@home.com', '12345'),
+(109, 'Larry', 'T2J 6B6', 'lwalter@aol.com', '12345'),
+(118, 'Hiediio', 'T3A 4ZG', 'hlopez@aol.com', '12345'),
+(121, 'Sean B', 'T2K 3E3', 'spineda@hotmail.com', '12345'),
+(122, 'Julita', 'T2K 6C5', 'jlippen@cadvision.co                              ', '12345'),
+(123, 'Pierre', 'T3G 2C6', 'pradicola@home.com                                ', '12345'),
+(130, 'Lula', 'T3E 3Z4', 'loates@aol.com                                    ', '12345'),
+(133, 'James', 'T3E 4A1', 'jreed@aol.com                                     ', '12345'),
+(138, 'John', 'T3E 5C7', 'johnSmith@hotmail.co                              ', '12345'),
+(142, 'Monica', 'T2J 6P3', 'mwaldman@aol.com                                  ', '12345'),
+(151, 'James Buchanan', 'A1A 1A1', 'abc@abc.com', '1234'),
+(152, 'Hilmi Ah', 't2y3p6', 'hilmi714@hotmail.com', 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `packages`
+--
+
+CREATE TABLE `packages` (
+  `PackageId` int(11) NOT NULL,
+  `PackageName` varchar(50) NOT NULL,
+  `PackagePhoto` varchar(256) DEFAULT NULL,
+  `PackageStartDate` datetime DEFAULT NULL,
+  `PackageEndDate` datetime DEFAULT NULL,
+  `PackageDesc` varchar(50) DEFAULT NULL,
+  `PackageBasePrice` decimal(19,4) NOT NULL,
+  `PackageAgencyCommission` decimal(19,4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `packages`
+--
+
+INSERT INTO `packages` (`PackageId`, `PackageName`, `PackagePhoto`, `PackageStartDate`, `PackageEndDate`, `PackageDesc`, `PackageBasePrice`, `PackageAgencyCommission`) VALUES
+(1, 'Caribbean New Year', 'https://imgur.com/Jh14XPb.png', '2017-12-25 00:00:00', '2017-01-04 00:00:00', 'Cruise the Caribbean & Celebrate the New Year.', '4800.0000', '400.0000'),
+(2, 'Polynesian Paradise', 'https://imgur.com/8pCwf5Q.png', '2016-12-12 00:00:00', '2016-12-20 00:00:00', '8 Day All Inclusive Hawaiian Vacation', '3000.0000', '310.0000'),
+(3, 'Asian Expedition', 'https://imgur.com/PZQ1Uyq.png', '2016-05-14 00:00:00', '2016-05-28 00:00:00', 'Airfaire, Hotel and Eco Tour.', '2800.0000', '300.0000'),
+(4, 'European Vacation', 'https://imgur.com/RCdfaiU.png', '2016-11-01 00:00:00', '2016-11-14 00:00:00', 'Euro Tour with Rail Pass and Travel Insurance', '3000.0000', '280.0000'),
+(5, 'North African Safari', 'https://i.imgur.com/t074NyZ.png', '2016-05-14 00:00:00', '2016-05-28 00:00:00', '9 Day Expedition In North Africa', '9000.0000', '400.0000'),
+(6, 'South American Adventure', 'https://imgur.com/nRpiSuZ.png', '2016-05-14 00:00:00', '2016-05-28 00:00:00', 'A 4 Day Trek Through South Americas Greatest Trail', '3000.0000', '200.0000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `triptypes`
+--
+
+CREATE TABLE `triptypes` (
+  `TripTypeId` varchar(1) NOT NULL,
+  `TTName` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `triptypes`
+--
+
+INSERT INTO `triptypes` (`TripTypeId`, `TTName`) VALUES
+('B', 'Business                 '),
+('G', 'Group                    '),
+('L', 'Leisure                  ');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `agencies`
+--
+ALTER TABLE `agencies`
+  ADD PRIMARY KEY (`AgencyId`);
+
+--
+-- Indexes for table `agents`
+--
+ALTER TABLE `agents`
+  ADD PRIMARY KEY (`AgentId`),
+  ADD KEY `AgencyId` (`AgencyId`);
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`BookingId`),
+  ADD KEY `BookingsCustomerId` (`CustomerId`),
+  ADD KEY `CustomersBookings` (`CustomerId`),
+  ADD KEY `PackageId` (`PackageId`),
+  ADD KEY `PackagesBookings` (`PackageId`),
+  ADD KEY `TripTypesBookings` (`TripTypeId`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`CustomerId`);
+
+--
+-- Indexes for table `packages`
+--
+ALTER TABLE `packages`
+  ADD PRIMARY KEY (`PackageId`);
+
+--
+-- Indexes for table `triptypes`
+--
+ALTER TABLE `triptypes`
+  ADD PRIMARY KEY (`TripTypeId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `agencies`
+--
+ALTER TABLE `agencies`
+  MODIFY `AgencyId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `agents`
+--
+ALTER TABLE `agents`
+  MODIFY `AgentId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `BookingId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1333;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `CustomerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=153;
+
+--
+-- AUTO_INCREMENT for table `packages`
+--
+ALTER TABLE `packages`
+  MODIFY `PackageId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `agents`
+--
+ALTER TABLE `agents`
+  ADD CONSTRAINT `agents_ibfk_1` FOREIGN KEY (`AgencyId`) REFERENCES `agencies` (`AgencyId`);
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`CustomerId`) REFERENCES `customers` (`CustomerId`),
+  ADD CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`PackageId`) REFERENCES `packages` (`PackageId`),
+  ADD CONSTRAINT `bookings_ibfk_3` FOREIGN KEY (`TripTypeId`) REFERENCES `triptypes` (`TripTypeId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
